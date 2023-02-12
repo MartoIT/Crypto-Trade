f3bb427d4842d772c4cf205241109c9875656438
Martin <88441320+MartoIT@users.noreply.github.com>
2023-02-12 14:44:08 +0200

    add dinamic navbar
    if user is login or register is have difren options then if is guest
    add authentication middleware

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 
 const app = express();
 const port = 5000;
 const router = require('./routers');
+const authMiddleware = require('./authMiddlewar/authMiddleware');
 
 const setupViewEngine = require('./config/viewEngine');
 const dataBase = require('./config/dataBase');
 
 setupViewEngine(app);
 app.use('/static', express.static('public') );
+app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
+app.use(authMiddleware.authentication)
 app.use(router);
 
 dataBase()
