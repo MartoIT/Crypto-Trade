dd91099f8f5db4a97d5eadd2a01ae20dae50ee8c
Martin <88441320+MartoIT@users.noreply.github.com>
2023-02-12 10:37:46 +0200

    initialize the project
    install libraries,
    add config folder.
    add mongoDB with mongoose.
    add router
    and start server with Express

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+const router = require('./routers');
+
+const setupViewEngine = require('./config/viewEngine');
+const dataBase = require('./config/dataBase');
+
+setupViewEngine(app);
+app.use(express.static('public') );
+app.use(express.urlencoded({extended: false}));
+app.use(router);
+
+dataBase()
+.then(() => console.log(`Server is listen on a port ${port}...`))
+.catch((err) => console.log(err.message));
