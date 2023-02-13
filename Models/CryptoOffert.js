b6bd588e9e9a0a47436c50f93dd8007e5b480f36
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-02-13 10:05:57 +0200

    add mongoose crypto schema

---
+++ Models/CryptoOffert.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const cryptoSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    payment:{
+        type: String,
+        required:[`crypto-wallet, credit-card, debit-card, paypal`],
+    },
+    
+
+})
+
+
+const Crypto = mongoose.model('Crypto', cryptoSchema);
+module.exports = Crypto;
+// •	Buy a crypto - a collection of Users (a reference to the User model)
+// •	Owner - object Id (a reference to the User model)
